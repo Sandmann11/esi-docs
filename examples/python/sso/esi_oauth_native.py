48a09489e02c8888103d43f3b614b9c2c8e03541
Arek P <sandmann@o2.pl>
2021-12-31 03:32:46 +0100

    initial

--- examples/python/sso/esi_oauth_native.py
+++ examples/python/sso/esi_oauth_native.py
@@ -1,24 +1,3 @@
-""" Python 3 native (desktop/mobile) OAuth 2.0 example.
-
-This example can be run from the command line and will show you how the
-OAuth 2.0 flow should be handled if you are a web based application.
-
-Prerequisites:
-    * Create an SSO application at developers.eveonline.com with the scope
-      "esi-characters.read_blueprints.v1" and the callback URL
-      "https://localhost/callback/". Note: never use localhost as a callback
-      in released applications.
-    * Have a Python 3 environment available to you (possibly by using a
-      virtual environment: https://virtualenv.pypa.io/en/stable/).
-    * Run pip install -r requirements.txt with this directory as your root.
-
-To run this example, make sure you have completed the prerequisites and then
-run the following command from this directory as the root:
-
->>> python esi_oauth_native.py
-
-then follow the prompts.
-"""
 import base64
 import hashlib
 import secrets
@@ -29,32 +8,17 @@ from shared_flow import handle_sso_token_response
 
 
 def main():
-    """ Takes you through a local example of the OAuth 2.0 native flow."""
-
-    print("This program will take you through an example OAuth 2.0 flow "
-          "that you should be using if you are building a desktop or mobile "
-          "application. Follow the prompts and enter the info asked for.")
 
     # Generate the PKCE code challenge
+    
     random = base64.urlsafe_b64encode(secrets.token_bytes(32))
     m = hashlib.sha256()
     m.update(random)
     d = m.digest()
     code_challenge = base64.urlsafe_b64encode(d).decode().replace("=", "")
-
-    client_id = input("Copy your SSO application's client ID and enter it "
-                      "here: ")
-
-    print("\nBecause this is a desktop/mobile application, you should use "
-          "the PKCE protocol when contacting the EVE SSO. In this case, that "
-          "means sending a base 64 encoded sha256 hashed 32 byte string "
-          "called a code challenge. This 32 byte string should be ephemeral "
-          "and never stored anywhere. The code challenge string generated for "
-          "this program is {} and the hashed code challenge is {}. \nNotice "
-          "that the query parameter of the following URL will contain this "
-          "code challenge.".format(random, code_challenge))
-
-    input("\nPress any key to continue:")
+    print(random)
+    print(code_challenge)
+    client_id = "b09e2123bd9346338dcecaf66e7f15b1"
 
     print_auth_url(client_id, code_challenge=code_challenge)
 
